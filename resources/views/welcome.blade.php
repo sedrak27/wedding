aa719a00742d57edafa45ef821f034c0bb31efbc
Sedrak Tsarukyan <sedrak.tsarukyan99@gmail.com>
2026-06-01 15:52:27 +0400

    latest fixes 7

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;1,300;1,400&family=Great+Vibes&family=Montserrat:wght@300;400;500&family=Noto+Serif+Armenian:wght@200;300;400&family=Playfair+Display:ital@0;1&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}?v={{ time() }}" />
   <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧡</text></svg>">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <script src="https://unpkg.com/@phosphor-icons/web@2.1.1"></script>
@@ -189,6 +189,6 @@
     <img src="{{ asset('images/couple/IMG_8364.webp') }}" alt="" />
   </div>
 
-  <script src="{{ asset('js/app.js') }}?v=1.6"></script>
+  <script src="{{ asset('js/app.js') }}?v={{ time() }}"></script>
 </body>
 </html>
